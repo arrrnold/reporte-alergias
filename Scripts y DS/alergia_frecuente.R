6fc791d0370e9b51312362baf30a753e8e78c28d
arrrnold <tm02arnold@gmail.com>
2023-06-08 21:43:07 -0600

    mas cambios

--- Scripts y DS/alergia_frecuente.R
+++ Scripts y DS/alergia_frecuente.R
@@ -2,24 +2,19 @@
 data <- read.csv("usuarios.csv", sep=";")
 
 # Calcular la frecuencia de las alergias
-
 # strsplit para dividir los nombres separados por coma
 # en elementos individuales
-
 ingredientes_separados <- strsplit(data$alergias, ",")
 
 # unlist para crear un vector para poner los nombres separados
 ingredientes <- unlist(ingredientes_separados)
-
 tabla_de_frecuencia <- table(ingredientes)
 
 # ordenar de menor a mayor las frecuencias
 alergias_ordenadas <- sort(tabla_de_frecuencia, decreasing = TRUE)
+alergia_mas_comun <- names(alergias_ordenadas)[1] # alergia ubicada en el #1
 
-alergia_mas_comun <- names(alergias_ordenadas)[1:5] # alergia ubicada en el #1
-
-# Imprimir el resultado
+# Mostrar el resultado
 cat("La alergia más común es:", alergia_mas_comun)
-
-# graficar
-plot(alergias_ordenadas)
+plot(alergias_ordenadas[1:8], xlab="Ingrediente",
+     ylab="Frecuencia", ylim=c(0,100))
